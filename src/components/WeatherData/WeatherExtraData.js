09c1dd628654f765899580b88457572032dd7851
Alberto Arraez Florez <aarraezf@cirsa.com>
2018-06-18 09:09:54 +0200

    test

---
+++ src/components/WeatherData/WeatherExtraData.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const WeatherExtraData = ({humidity, wind}) => (
+    <div>
+        <span>{`${humidity}  % `}</span>
+        <span>{`${wind} km/h`}</span>
+    </div>
+);
+
+WeatherExtraData.propTypes =
+{
+    humidity : PropTypes.number.isRequired,
+    wind : PropTypes.string.isRequired,
+}
+
+export default WeatherExtraData;
