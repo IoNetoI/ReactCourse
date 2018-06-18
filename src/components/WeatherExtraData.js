4913f2699ac1f873112ac6ac864d551884014d56
Alberto Arraez Florez <aarraezf@cirsa.com>
2018-06-18 08:16:27 +0200

    push

--- src/components/WeatherExtraData.js
+++ src/components/WeatherExtraData.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const WeatherExtraData = ({humidity, wind}) => (
     <div>
@@ -6,5 +7,11 @@ const WeatherExtraData = ({humidity, wind}) => (
         <span>{`${wind} km/h`}</span>
     </div>
 );
+
+WeatherExtraData.propTypes =
+{
+    humidity : PropTypes.number.isRequired,
+    wind : PropTypes.string.isRequired,
+}
 
 export default WeatherExtraData;
