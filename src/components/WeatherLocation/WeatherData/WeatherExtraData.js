2ec54a1c5409f14ace78f30c00f85fa0c6707f4d
IoNetoI <40233252+IoNetoI@users.noreply.github.com>
2019-07-23 14:43:39 +0200

    Add files via upload

--- src/components/WeatherLocation/WeatherData/WeatherExtraData.js
+++ src/components/WeatherLocation/WeatherData/WeatherExtraData.js
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 import './style.css';
 
 const WeatherExtraData = ({humidity, wind}) => (
-    <div>
-        <span>{`${humidity}  % `}</span>
-        <span>{`${wind} km/h`}</span>
+    <div className='weatherExtraDataCont'>
+        <span className="extraInfoText">{`Humedad ${humidity}  % `}</span>
+        <span className="extraInfoText">{`Viento ${wind} km/h`}</span>
     </div>
 );
 
-WeatherExtraData.propTypes =
+WeatherExtraData.PropTypes =
 {
     humidity : PropTypes.number.isRequired,
-    wind : PropTypes.string.isRequired,
+    wind : PropTypes.number.isRequired,
 }
 
 export default WeatherExtraData;
