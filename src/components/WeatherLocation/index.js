59a6eb8c3fa6bc43961cf792aae5f8286ff94ec4
Alberto Arraez Florez <aarraezf@cirsa.com>
2019-07-23 20:33:51 +0200

    curso react

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -10,10 +10,11 @@ const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
 const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
 
 const data = {
+    city:"Terrassa",
     temperature :5,
     weatherState: SUN,
     humidity:10,
-    wind:10
+    wind:'10'
 }
 
 class WeatherLocation  extends Component {
@@ -21,13 +22,35 @@ class WeatherLocation  extends Component {
     constructor(){
         super();//constructor base react
         this.state ={
-            city: "Terrassa",
+            city: data.city,
             data : data,
         };
     }
 
+getWeatherState = weather_data => {
+    return SUN;
+}
+
+getData = weather_data => {
+    const {humidity, temp} = weather_data.main;
+    const {speed} = weather_data.wind;
+    const city = weather_data.name;
+    const weatherState = this.getWeatherState(weather_data);
+
+    const data = {
+        city,
+        humidity,
+        temperature : temp,
+        weatherState,
+        wind:`${speed} m/s`
+    }
+
+    return data;
+}
+
     setDataToView = (datos) =>{
         this.setState({
+            city: datos.city,
             data:datos,
         });
     }
@@ -38,12 +61,7 @@ class WeatherLocation  extends Component {
             return resolve.json();
             })
             .then((response) => {
-                data.city = response.name;
-                data.humidity = response.main.humidity;
-                data.temperature= response.main.temp;
-                data.wind = response.wind.speed;
-
-                this.setDataToView(data);
+                this.setDataToView(this.getData(response));
             });   
     }
 
